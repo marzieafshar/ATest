fa44d3098ed77996e4211358bb5962ffeaa00b79
Marzie <marzieafshar2002@yahoo.com>
2021-04-25 01:43:47 +0430

    add something and delete something else

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
         System.out.println("سلام دنیا");
+        System.out.println("hi world");
+        System.out.println("bye world");
     }
 }
